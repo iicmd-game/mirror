e1569c8e9c3c88c70e924d3863e59ef2861fb218
Khassasin <51829070+Khassasin@users.noreply.github.com>
2020-06-30 13:35:53 +0400

    CheckPoints

---
+++ Mirror's Edge/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public bool triggered = false;
+    public static GameObject[] CheckPointList;
+    GameObject respawn;
+
+    void Start()
+    {
+        CheckPointList = GameObject.FindGameObjectsWithTag("CheckPoint");
+        respawn = GameObject.FindGameObjectWithTag("Respawn");
+    }
+
+    private void ActivateCheckPoint()
+    {
+        foreach (GameObject cp in CheckPointList)
+        {
+            cp.GetComponent<CheckPoint>().triggered = false;
+        }
+
+        triggered = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ActivateCheckPoint();
+        }
+    }
+
+    public GameObject CheckPointPosition()
+    {
+        GameObject result = respawn;
+        if (CheckPointList != null)
+        {
+            foreach (GameObject cp in CheckPointList)
+            {
+                if (cp.GetComponent<CheckPoint>().triggered)
+                {
+                    result = cp;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
