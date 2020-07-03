38f3aa784366bde0f9edf46f7fe7ce138b3dc251
Khassasin <51829070+Khassasin@users.noreply.github.com>
2020-07-03 18:45:18 +0400

    Шаги

---
+++ Mirror's Edge/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Faith_controller fc;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        Footsteps();
+    }
+    public void Footsteps()
+    {
+        if (fc.isGrounded && !fc.isRoll && fc.move != 0)
+        {
+            StartFootsteps();
+        }
+        else
+        {
+            StopFootsteps();
+        }
+    }
+
+    void StartFootsteps()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void StopFootsteps()
+    {
+        audioSource.Stop();
+    }
+}
